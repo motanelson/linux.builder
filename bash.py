6fc63e775c731fbf69c46e04b3c56a52d1a5657d
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-19 16:35:27 +0000

    Add files via upload

--- bash.py
+++ bash.py
@@ -1,6 +1,60 @@
 import os
 import time
 from datetime import datetime
+def retb(c,value,n):
+    a=b""
+    a=c*value
+    f1=open(n,"wb")
+    f1.write(a)
+    f1.close()
+    
+def retk(c,value):
+    a=b""
+    a=c*value
+    return a
+def retm(c,value):
+    a=b""
+    a=c*value
+    return a
+def retg(c,value):
+    a=b""
+    a=c*value
+    return a
+def ddds():
+    k=1024
+    m=k*k
+    g=m*k
+    cc=b"\x00"
+    print("\ngive me the output file name ? ")
+    kk=b""
+    mm=b""
+    gg=b""
+    kk=retk(cc,k)
+    mm=retm(kk,k)
+    gg=retg(mm,k)
+    v=b""
+    n=input()
+    n=n.strip()
+    print("\ngive me the unit 0=bytes 1=k kilo 2=M mega 3=G gigas ? ")
+    nn=input()
+    nn=nn.strip()
+    nn=int(nn)
+    if nn>-1 and nn<4:
+        print("\ngive me the unit ? ")
+        nnn=input()
+        nnn=nnn.strip()
+        nnn=int(nnn)
+        if nn==0:
+            retb(cc,nnn,n)
+        if nn==1:
+            retb(kk,nnn,n)
+        if nn==2:
+            retb(mm,nnn,n)
+        if nn==3:
+            retb(gg,nnn,n)
+
+
+
 def editline(i):
     r=""
     if os.path.exists(i):
@@ -142,6 +196,9 @@ while(True):
         formatted_date = today.strftime("%d-%b-%y")
         t=False
         print(formatted_date)
+    if iii=="dd":
+        t=False
+        ddds()
     if t:
         os.system(i)
     
